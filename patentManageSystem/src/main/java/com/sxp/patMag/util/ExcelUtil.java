c7bce89343f0155d7692e2c088c07f216c1befc9
WangLoveDong <1015560223@qq.com>
2019-11-22 17:01:13 +0800

    专利查询1.2

--- patentManageSystem/src/main/java/com/sxp/patMag/util/ExcelUtil.java
+++ patentManageSystem/src/main/java/com/sxp/patMag/util/ExcelUtil.java
@@ -1,10 +1,11 @@
 package com.sxp.patMag.util;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -58,6 +59,115 @@ public class ExcelUtil {
         os.flush();
         os.close();
     }
+
+
+
+    public static void createWorkbook(List<Map<String, Object>> list, String[] keys, String[] columnNames, /*String filePath*/ HttpServletResponse response) throws IOException {
+    /*    if (filePath == null) {
+            return;
+        }*/
+        //创建工作簿
+        SXSSFWorkbook workbook = new SXSSFWorkbook();
+        //创建sheet
+        Sheet sheet = workbook.createSheet(list.get(0).get("sheetName").toString());
+        //创建行
+        Row row = sheet.createRow(0);
+        //设置列名
+        for (int i = 0; i < columnNames.length; i++) {
+            Cell cell = row.createCell(i);
+            cell.setCellValue(columnNames[i]);
+        }
+        //设置每行每列的值
+        for (int i = 1; i < list.size(); i++) {
+            // Row 行,Cell 方格 , Row 和 Cell 都是从0开始计数的
+            // 创建一行，在页sheet上
+            Row row1 = sheet.createRow(i);
+            // 在row行上创建一个方格
+            for (int j = 0; j < keys.length; j++) {
+                Cell cell = row1.createCell(j);
+                cell.setCellValue(list.get(i).get(keys[j]) == null ? " " : list.get(i).get(keys[j]).toString());
+            }
+        }
+        //准备将Excel的输出流通过response输出到页面下载
+        //八进制输出流
+        response.setContentType("application/octet-stream");
+        //设置导出Excel的名称
+        response.setHeader("Content-disposition", "attachment;filename=" + "wangshuo.xlsx");
+        //刷新缓冲
+        response.flushBuffer();
+        //workbook将Excel写入到response的输出流中，供页面下载该Excel文件
+        workbook.write(response.getOutputStream());
+       // workbook.write(os);
+      //  os.flush();
+        workbook.close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Excel表格输出下载界面
+     *
+     * @param response    HttpServletResponse对象
+     * @param excelData   Excel表格的数据，封装为List<List<String>>
+     * @param sheetName   sheet的名字
+     * @param fileName    导出Excel的文件名
+     * @param columnWidth Excel表格的宽度，建议为15
+     * @throws IOException 抛IO异常
+     */
+       public static void exportExcel(HttpServletResponse response,
+                                   List<List<String>> excelData,
+                                   String sheetName,
+                                   String fileName,
+                                   int columnWidth) throws IOException {
+
+        //声明一个工作簿
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        //生成一个表格，设置表格名称
+        HSSFSheet sheet = workbook.createSheet(sheetName);
+        //设置表格列宽度
+        sheet.setDefaultColumnWidth(columnWidth);
+        //写入List<List<String>>中的数据
+        int rowIndex = 0;
+        for (List<String> data : excelData) {
+            //创建一个row行，然后自增1
+            HSSFRow row = sheet.createRow(rowIndex++);
+            //遍历添加本行数据
+            for (int i = 0; i < data.size(); i++) {
+                //创建一个单元格
+                HSSFCell cell = row.createCell(i);
+                //创建一个内容对象
+                HSSFRichTextString text = new HSSFRichTextString(data.get(i));
+                //将内容对象的文字内容写入到单元格中
+                cell.setCellValue(text);
+            }
+        }
+        //准备将Excel的输出流通过response输出到页面下载
+        //八进制输出流
+        response.setContentType("application/octet-stream");
+        //设置导出Excel的名称
+        response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+        //刷新缓冲
+        response.flushBuffer();
+        //workbook将Excel写入到response的输出流中，供页面下载该Excel文件
+        workbook.write(response.getOutputStream());
+        //关闭workbook
+        workbook.close();
+    }
+
+
+
+
+
+
+
 
     /**
      * 读取excel 第1张sheet （xls和xlsx）
