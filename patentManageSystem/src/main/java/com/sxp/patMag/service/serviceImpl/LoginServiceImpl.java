fd7187f89e02436a191aa8f5d1de8660e0e8dffd
zhu956645239 <xx633633>
2019-11-26 21:29:17 +0800

    规范，修改

--- patentManageSystem/src/main/java/com/sxp/patMag/service/serviceImpl/LoginServiceImpl.java
+++ patentManageSystem/src/main/java/com/sxp/patMag/service/serviceImpl/LoginServiceImpl.java
@@ -6,10 +6,12 @@ import com.sxp.patMag.entity.User;
 import com.sxp.patMag.service.LoginService;
 import com.sxp.patMag.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+
 /**
  * @author： Jude
  * @date:2019/11/26
@@ -17,17 +19,21 @@ import java.util.List;
  */
 
 @Service
-public class LoginServiceImpl implements LoginService  {
+public class LoginServiceImpl implements LoginService {
 
     @Resource
     private LoginMapper loginMapper;
     @Autowired
-    private RedisUtil redis ;
+    private RedisUtil redis;
 
     @Autowired
     private WeLogFile weLogFile;
     @Autowired
     private HistoryAOP historyAOP;
+    @Value("${expireTime}")
+    private Integer expireTime;
+
+
     @Override
     public GeneralResult login(User user) {
 
@@ -36,24 +42,22 @@ public class LoginServiceImpl implements LoginService  {
 //        user.setUserPassword(user.getUserPassword().trim());
 
 
-
-
         //判断用户名和密码是否正确
-        if (user==null){
+        if (user == null) {
             return GeneralResult.build(1, "用户名或密码为空");
         }
         if (StringUtils.isNull(user.getUserName())) {
 
             return GeneralResult.build(1, "用户名或密码为空");
         }
-        if (StringUtils.isNull(user.getUserPassword())){
+        if (StringUtils.isNull(user.getUserPassword())) {
             return GeneralResult.build(1, "用户名或密码为空");
         }
-        if ( user.getUserPassword().length()>16 ||user.getUserName().length()>16 ){
+        if (user.getUserPassword().length() > 16 || user.getUserName().length() > 16) {
             return GeneralResult.build(1, "用户名或密码长度超过了最大长度");
         }
 
-        List<User> list = loginMapper.CheckUser(user);
+        List<User> list = loginMapper.checkUser(user);
         if (list == null || list.size() == 0) {
             //返回登录失败
             return GeneralResult.build(1, "用户名或密码不正确");
@@ -65,29 +69,29 @@ public class LoginServiceImpl implements LoginService  {
         user.setUserPassword(null);
         historyAOP.setUser(user);
         weLogFile.setUser1(user);
-         //权限存储
-         user.setUserRole(list.get(0).getUserRole());
-         user.setUserId(list.get(0).getUserId());
+        //权限存储
+        user.setUserRole(list.get(0).getUserRole());
+        user.setUserId(list.get(0).getUserId());
         //把用户信息保存到redis，key就是token，value就是用户信息
         redis.set("UserLogin" + ":" + token, JsonUtils.objectToJson(user));
         //设置key的过期时间
-        redis.expire("UserLogin" + ":" + token, 1800);
+        redis.expire("UserLogin" + ":" + token, expireTime);
         //返回登录成功，其中要把token返回。
-        return GeneralResult.build(0, user.getUserRole(),token);
+        return GeneralResult.build(0, user.getUserRole(), token);
     }
+
     @Override
     public GeneralResult getUserByToken(String token) {
         String json = (String) redis.get("UserLogin" + ":" + token);
-        if ( json==null||json.length()==0) {
+        if (json == null || json.length() == 0) {
             return GeneralResult.build(1, "用户登录已经过期");
         }
         //重置Session的过期时间
-        redis.expire("UserLogin" + ":" + token, 1800);
+        redis.expire("UserLogin" + ":" + token, expireTime);
         //把json转换成User对象
         User user = JsonUtils.jsonToPojo(json, User.class);
         return GeneralResult.ok(user);
     }
-
 
 
 }
--- patentManageSystem/src/main/java/com/sxp/patMag/util/HistoryAOP.java
+++ patentManageSystem/src/main/java/com/sxp/patMag/util/HistoryAOP.java
@@ -4,6 +4,7 @@ import com.sxp.patMag.annotation.Monitor;
 import com.sxp.patMag.entity.History;
 import com.sxp.patMag.entity.Patent;
 import com.sxp.patMag.entity.User;
+import com.sxp.patMag.enums.ProcessEnum;
 import com.sxp.patMag.service.HistoryService;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -67,7 +68,8 @@ public class HistoryAOP {
         String value = null;
         if (monitor != null) {
              value = monitor.value();
-            history.setHtOperation(value);//保存获取的操作
+            history.setHtOperation(value);
+            //保存获取的操作
         }
         //获取请求的类名
         String className = joinPoint.getTarget().getClass().getName();
@@ -83,156 +85,90 @@ public class HistoryAOP {
         history.setHtDate(date);
         history.setHtId(UUID.getUUID());
         history.setHtUserId(user.getUserId());
-           System.out.println(params);
+        //   System.out.println(params);
            //解析参数中的patent 成对象
         Patent patent  = JSON.parseObject( params.substring(1,params.length()-1), Patent.class);
            //从reids获取之前的专利信息
-        String old_obj =  redis.get("Patent" + ":" + patent.getPatentId())!=null ? redis.get("Patent" + ":" +patent.getPatentId()).toString() :null;
+        String oldObj =  redis.get("Patent" + ":" + patent.getPatentId())!=null ? redis.get("Patent" + ":" +patent.getPatentId()).toString() :null;
 
-        System.out.println(old_obj);
+       // System.out.println(old_obj);
 
         //解析之前的专利为对象
-        Patent old_patent = JSON.parseObject( old_obj, Patent.class);;
+        Patent oldPatent = JSON.parseObject( oldObj, Patent.class);;
 
         //将新的对象放到redis
         redis.set("Patent" + ":" +patent.getPatentId(), JSON.toJSON(patent));
         redis.expire("Patent" + ":" +patent.getPatentId(),  86400);
-        if (value.equals("新建专利")){
+        if (ProcessEnum.NEW.equals(value)){
             history.setHtPatentId(patent.getPatentId());
-
-            StringBuilder stringBuilder = new StringBuilder();
-
-            if (patent.getApplyNumber()!=null){
-                stringBuilder.append("申请号 ："+patent.getApplyNumber()+"  ");
-            }
-            if(patent.getCaseNumber()!=null){
-                stringBuilder.append("案件文号 ："+patent.getCaseNumber()+"  ");
-            }
-            if(patent.getPatentName()!=null){
-                stringBuilder.append("专利名 ："+patent.getPatentName()+"  ");
-            }
-            if(patent.getCreatePerson()!=null){
-                stringBuilder.append("发明人 ："+patent.getCreatePerson()+"   ");
-            }
-            if(patent.getApplyPerson()!=null){
-                stringBuilder.append("申请人 ："+patent.getApplyPerson()+"   ");
-            }
-
-            history.setHtNewItem(stringBuilder.toString());
+            history.setHtNewItem(getHistory(patent));
             history.setHtOldItem("无");
             history.setHtProcess(value);
             history.setHtOperation("新建专利");
-        }else if(value.equals("专利认领")){
+        }
+        if(ProcessEnum.CLAIM.equals(value)){
             history.setHtPatentId(patent.getPatentId());
             history.setHtNewItem("认领人 : "+patent.getWritePerson());
             history.setHtOldItem("认领人 : 无");
             history.setHtProcess(value);
-            history.setHtOperation("修改了专利撰写人");
-        }else if(value.equals("审核")){
+            history.setHtOperation("修改撰写人");
+        }
+        if(ProcessEnum.CHECK.equals(value)){
             if (patent.getPatentClaim()=="0"){
+                System.out.println("初审");
                 history.setHtPatentId(patent.getPatentId());
                 history.setHtNewItem("专利进度 : "+patent.getPatentSchedule());
                 history.setHtOldItem("专利进度 : 未审核");
-                history.setHtProcess(value);
-                history.setHtOperation("初审");
+                history.setHtProcess("初审");
+                history.setHtOperation(value);
             }
             if (patent.getPatentClaim()=="1"){
+                System.out.println("复审");
                 history.setHtPatentId(patent.getPatentId());
                 history.setHtNewItem("专利进度 : "+patent.getPatentSchedule());
                 history.setHtOldItem("专利进度 : 编写中");
-                history.setHtProcess(value);
-                history.setHtOperation("复审");
+                history.setHtProcess("复审");
+                history.setHtOperation(value);
             }
-        } else if(value.equals("修改字段")){
-
-      /*  Object obj =  redis.get(patent.getPatentId());
-*/
-           /* Patent pobj  = JSON.parseObject(  obj.toString() , Patent.class);*/
-            //Patent obj  =(Patent) redis.get(patent.getPatentId());
-
-            StringBuilder stringBuilder = new StringBuilder();
-
-            if (patent.getApplyNumber()!=null){
-                stringBuilder.append("申请号 ："+patent.getApplyNumber()+"  ");
-            }
-            if(patent.getCaseNumber()!=null){
-                stringBuilder.append("案件文号 ："+patent.getCaseNumber()+ "   ");
-            }
-            if(patent.getPatentName()!=null){
-                stringBuilder.append("专利名 ："+patent.getPatentName()+"   ");
-            }
-            if(patent.getPatentRemarks()!=null){
-                stringBuilder.append("备注 ："+patent.getPatentRemarks()+"  ");
-            }
-
-            StringBuilder stringBuilder2 = new StringBuilder();
-
-            if (old_patent.getApplyNumber()!=null){
-                stringBuilder2.append("申请号 ："+old_patent.getApplyNumber()+"  ");
-            }
-            if(old_patent.getCaseNumber()!=null){
-                stringBuilder2.append("案件文号 ："+old_patent.getCaseNumber()+"  ");
-            }
-            if(old_patent.getPatentName()!=null){
-                stringBuilder2.append("专利名 ："+old_patent.getPatentName()+"  ");
-            }
-            if(old_patent.getPatentRemarks()!=null){
-                stringBuilder2.append("备注 ："+old_patent.getPatentRemarks()+"  ");
-            }
+        }
+        if(ProcessEnum.UPDATE.equals(value)){
 
             history.setHtPatentId(patent.getPatentId());
-            history.setHtNewItem(stringBuilder.toString());
-            history.setHtOldItem(stringBuilder2.toString());
+            history.setHtNewItem(getHistory(patent));
+            history.setHtOldItem(getHistory(oldPatent));
             history.setHtProcess(value);
             history.setHtOperation("修改字段");
         }
-        /*
-        else if(){
-            //认领
-
-
-
-        }else if(){
-            //提交
-
-
-        }else if(){
-
-        }*/
-
         historyService.insertHistory(history);
         //获取用户名
         Object proceed = joinPoint.proceed();
         return proceed;
     }
 
-    //@AfterReturning(returning = "ret",pointcut = "getAction()")
-    public void doAfter(JoinPoint joinPoint, Object ret) throws Exception {
-        Integer result = (Integer) ret;
+public static String getHistory(Patent patent){
 
-        System.out.println("方法执行后返回");
-        System.out.println(result);
+    StringBuilder stringBuilder = new StringBuilder();
 
+    if (patent.getApplyNumber()!=null){
+        stringBuilder.append("申请号 ："+patent.getApplyNumber()+"  ");
     }
-
-
-   // @Before("getAction()")
-    public void doBefore(JoinPoint joinPoint)  {
-
-        System.out.println("方法执行前");
-  /*      //请求的参数
-        Object[] args = joinPoint.getArgs();
-        //将参数所在的数组转换成json
-
-        String params = JSON.toJSONString(args);
-
-        Patent patent = JSON.parseObject(params.substring(1,params.length()-1),Patent.class);
-
-        redis.set(patent.getPatentId(), JSON.toJSON(patent));
-
-        System.out.println(params);
-        */
+    if(patent.getCaseNumber()!=null){
+        stringBuilder.append("案件文号 ："+patent.getCaseNumber()+ "   ");
     }
+    if(patent.getPatentName()!=null){
+        stringBuilder.append("专利名 ："+patent.getPatentName()+"   ");
+    }
+    if(patent.getPatentRemarks()!=null){
+        stringBuilder.append("备注 ："+patent.getPatentRemarks()+"  ");
+    }
+    if(patent.getWritePerson()!=null){
+        stringBuilder.append("撰写人 ："+patent.getWritePerson()+"   ");
+    }
+    if(patent.getCreatePerson()!=null){
+        stringBuilder.append("发明人 ："+patent.getCreatePerson()+"   ");
+    }
+        return stringBuilder.toString();
+}
 
 
 }
