b2ac3d3492b087ec55f51897ef579cde66f0b539
zhu956645239 <xx633633>
2019-11-27 10:13:41 +0800

    删掉注释

--- patentManageSystem/src/main/java/com/sxp/patMag/service/serviceImpl/InsertServiceImpl.java
+++ patentManageSystem/src/main/java/com/sxp/patMag/service/serviceImpl/InsertServiceImpl.java
@@ -27,37 +27,6 @@ public class InsertServiceImpl implements InsertService {
     @Override
     public GeneralResult insertPatent(@Valid Patent patent) {
 
-
-/*
-        if (null == patent.getCreatePerson() || "".equals(patent.getCreatePerson())) {
-            return GeneralResult.build(1, "发明人不能为空");
-        }
-        if (null == patent.getPatentName() || "".equals(patent.getPatentName())) {
-            return GeneralResult.build(1, "专利名称不能为空");
-        }
-        if (null == patent.getApplyPerson() || "".equals(patent.getApplyPerson())) {
-            return GeneralResult.build(1, "申请人不能为空");
-        }
-
-
-        if (patent.getApplyNumber() != null && patent.getApplyNumber().length() > 16) {
-            return GeneralResult.build(1, "申请号长度过长", null);
-        }
-        if (patent.getPatentName() != null && patent.getPatentName().length() > 50) {
-            return GeneralResult.build(1, "专利名字长度过长", null);
-        }
-        if (patent.getApplyPerson() != null && patent.getApplyPerson().length() > 50) {
-            return GeneralResult.build(1, "申请人长度过长", null);
-        }
-        if (patent.getCreatePerson() != null && patent.getCreatePerson().length() > 50) {
-            return GeneralResult.build(1, "发明人长度过长", null);
-        }
-        if (patent.getCaseNumber() != null && patent.getCaseNumber().length() > 16) {
-            return GeneralResult.build(1, "案例号长度过长", null);
-        }
-*/
-
-
         int a = insertMapper.insertPatent(patent);
         if (a > 0) {
                return GeneralResult.build(0, "success");
