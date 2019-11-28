73634d7fd6cee9c2d2daeff701fc963771f07e41
WangLoveDong <1015560223@qq.com>
2019-11-28 10:56:51 +0800

    1.8999

---
+++ patentManageSystem/src/main/java/com/sxp/patMag/util/CatchExection.java
@@ -0,0 +1,37 @@
+package com.sxp.patMag.util;
+
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @description:
+ * @author: Andy
+ * @time: 2019/11/28 10:38
+ */
+@ControllerAdvice
+public class CatchExection {
+
+
+
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseBody
+    public GeneralResult catchExection(){
+        return  GeneralResult.build(1,"登陆不成功");
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer  embeddedServletContainerCustomizer(){
+
+        EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer = (container) -> {
+            container.addErrorPages( new ErrorPage( HttpStatus.NOT_FOUND,"/e404"));
+        };
+        return  embeddedServletContainerCustomizer;
+    }
+}
