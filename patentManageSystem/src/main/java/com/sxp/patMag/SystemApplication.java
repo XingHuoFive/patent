16c65ab173fffdaf1c8e1d7fc49f566d38042740
zhu956645239 <xx633633>
2019-11-29 10:14:44 +0800

    启动类 注解

--- patentManageSystem/src/main/java/com/sxp/patMag/SystemApplication.java
+++ patentManageSystem/src/main/java/com/sxp/patMag/SystemApplication.java
@@ -3,11 +3,13 @@ package com.sxp.patMag;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @MapperScan("com.sxp.patMag.dao")
 @SpringBootApplication
+@EnableTransactionManagement
 public class SystemApplication {
         public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class,args);
