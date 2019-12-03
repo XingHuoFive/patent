438f75c407ad8c0f516b7e64d9faf3ae45ae2508
zhangguofeng-del <zgf19961012@sina.com>
2019-12-03 17:27:33 +0800

    zgf1726

--- patentManageSystem/src/main/java/com/sxp/patMag/controller/AdminController.java
+++ patentManageSystem/src/main/java/com/sxp/patMag/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.sxp.patMag.controller;
 
 import com.sxp.patMag.entity.Patent;
+import com.sxp.patMag.entity.User;
 import com.sxp.patMag.service.AdminService;
 import com.sxp.patMag.util.GeneralResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 /**
  * @Author： Guofengzhang
@@ -20,32 +22,51 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
-    /** 审核新建的专利 */
+    /**
+     * 审核新建的专利
+     */
     @RequestMapping(value = "/checkPatent", method = RequestMethod.POST)
     @ResponseBody
     public GeneralResult checkPatent(@RequestBody Patent patent) {
         return adminService.checkPatent(patent);
     }
 
-    /** 根据专利的id查询专利所属的交底书 */
-    @RequestMapping(value = "/selectAllFilesByPatentId/{patentId}", method = RequestMethod.POST)
-    @ResponseBody
-    public GeneralResult selectAllFilesByPatentId(@PathVariable("patentId") String patentId) {
-        return adminService.selectAllFilesByPatentId(patentId);
-    }
-
-    /** 管理员读取日志 */
+    /**
+     * 管理员读取日志
+     */
     @RequestMapping(value = "/readLogFile/{role}", method = RequestMethod.POST)
     @ResponseBody
     public GeneralResult readLogFile(@PathVariable("role") String role, HttpServletResponse response) {
         return adminService.readLogFile(role, response);
     }
 
-    @RequestMapping(value = "/getLogPath/{role}", method = RequestMethod.POST)
+    /**
+     * 修改专利的备注状态，模拟通知
+     *
+     * @param patent 专利
+     * @return 修改结果
+     */
+    @RequestMapping(value = "/updatePatentRemarkView", method = RequestMethod.POST)
     @ResponseBody
-    public GeneralResult getLogPath(@PathVariable("role") String role) {
-        return adminService.getLogPath(role);
+    public GeneralResult updatePatentRemarkView(@Valid @RequestBody Patent patent) {
+        return adminService.updatePatentRemarkView(patent);
     }
 
+    /**
+     * 登录后显示通知
+     *
+     * @param user 登录者信息
+     * @return 通知列表
+     */
+    @RequestMapping(value = "/showPatentNotice", method = RequestMethod.POST)
+    @ResponseBody
+    public GeneralResult showPatentNotice(@Valid @RequestBody User user) {
+        return adminService.showPatentNotice(user);
+    }
 
+//    @RequestMapping(value = "/getLogPath/{role}", method = RequestMethod.POST)
+//    @ResponseBody
+//    public GeneralResult getLogPath(@PathVariable("role") String role) {
+//        return adminService.getLogPath(role);
+//    }
 }
